14cdd54cf3141b94059ebf9ae7e1d5adce2b6bc8
Raymond Pistoresi <raymondpistoresi@Raymonds-MacBook-Pro.local>
2019-01-27 16:36:00 -0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainNavComponent,
+    MainContentComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
